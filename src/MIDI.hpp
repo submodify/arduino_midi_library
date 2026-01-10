59265916bfff5b07ab4bbd7e93c7aed7dde7abe7
submodify <submodify@hotmail.com>
2026-01-10 19:59:10 +0000

    Update MIDI.hpp
    Added doc heading

--- src/MIDI.hpp
+++ src/MIDI.hpp
@@ -1126,7 +1126,11 @@ bool MidiInterface<Transport, Settings, Platform>::parse()
         }
     }
 }
-	
+
+/*! \brief Convert Note On Velocity 0 events to Note off (default behaviour).
+ \param convert - Sets whether Note On Velocity 0 is converted to a Note Off (default = true).
+ \param off_velocity - Optionally sets the Release Velocity (0-127) of the converted Note Off (default = 0). 
+*/
 template<class Transport, class Settings, class Platform>
 inline void MidiInterface<Transport, Settings, Platform>::convertNullVelocityNoteOnToNoteOff(bool convert, uint8_t off_velocity)
 {
